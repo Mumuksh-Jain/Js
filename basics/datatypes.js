6887c947abaf7ed11d6245205c62b58bb92d9831
Mumuksh-Jain <mumukshujain2466@gmail.com>
2025-07-04 13:47:43 +0000

    Config

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+//alert(3+3) cant be used as we are not in browser
+console.log(3+3)// code should be easy to read
+
+let name="Mumukshu"
+let age=174
+// number => 2^(2 to 53)
+// bigint
+//string => ""
+//boolean
+//null => stand alone value (empty value)
+// undefinrd => no value is given
+// symbol => unique
+// object
+console.log(typeof(age));
+console.log(typeof(null))
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId= 14553
+let accountmail= "mumukshujain2466@gmail.com"
+var accountpass="12345"
+accountcity="jaipur"
+console.log(accountId);
+accountmail="ha.aj"
+accountpass="212121"
+accountcity="banaras"
+console.table([accountId, accountmail, accountpass, accountcity])
+//do not use var
+// prefer not to use var because of issue in block scope and functional scope

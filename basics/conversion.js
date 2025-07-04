12b4a3bf082ec0e3a1021d6f549a48dfb6a15bf3
Mumuksh-Jain <mumukshujain2466@gmail.com>
2025-07-04 17:56:07 +0000

    conversionandcomparison

---
+++ basics/conversion.js
@@ -0,0 +1,22 @@
+let score =3
+let scoree=String(score)
+console.log(scoree);
+console.log(typeof (scoree));
+console.log(typeof (score));
+ let tr ="1"
+let trr=Boolean(tr)
+ console.log(trr);
+ console.log("1"+2+2);// the first value defines the datatype of the output
+ console.log(1+2+"2");
+ 
+ console.log(2>1);
+ console.log(2/1==0)
+ console.log(null>=0);
+ console.log(undefined==0);
+ //  === checks datatype
+ // these null and undefined are not used 
+ console.log("2"==2);
+ 
+ 
+
+

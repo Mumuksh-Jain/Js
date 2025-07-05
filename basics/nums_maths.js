7542cc9a91958b15b20396c95057d41e3fc2e17d
Mumuksh-Jain <mumukshujain2466@gmail.com>
2025-07-05 09:50:47 +0000

    nums_maths

---
+++ basics/nums_maths.js
@@ -0,0 +1,35 @@
+const score= 400
+console.log(score);
+const balance =new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+// to fixed => zeroes after decimal
+
+const num= 23.896
+console.log(num.toPrecision(2));
+// to precision gives the value up to given terms
+// to loccalstring for conversion in indian or other 
+console.log(Math);
+console.log(Math.abs(-4)); // negative to positive
+// Math.round() gives round off
+// ceil e.g  Math.ceil(4.2)=5
+// floor  e.g  Math.floor(4.2)=4
+console.log(Math.min(3,4,6,7));//gives min value
+console.log(Math.random());  // random between 0 to 1
+console.log(Math.random()*10); // random *10
+console.log((Math.random()*10)+1); // ensures value greater than 1
+
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min)); // 1 is added bcoz floor(0.1)=0
+
+
+
+
+
+
+

d13ad165ad6666f217789aca623e7d0053c64bbd
Mumuksh-Jain <mumukshujain2466@gmail.com>
2025-07-07 05:47:53 +0000

    while

---
+++ iterations/whileloop.js
@@ -0,0 +1,23 @@
+// while and do while
+
+let i=0
+while(i<=20)
+   { console.log(i);
+    i++;
+   }
+
+let a=["a","b","c"]
+let j=0
+while(j<a.length)
+   { console.log(a[j]);
+    j++ ;
+   }
+let score =1
+do {
+    console.log(score);
+    score++  
+}
+while(score<=5);
+
+// while loop will not execute if condition is false
+// do while will execute once even if condition is false
